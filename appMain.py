12405e59906e2ef57a17fa419300c6cbf58f10f2
aromal-baby <aromalbaby365@gmail.com>
2024-12-07 02:33:30 +0100

    07-12

--- appMain.py
+++ appMain.py
@@ -7,6 +7,10 @@ def index():
     return render_template('UI.htm')
 
 
+@app.route('/AboutUS')
+def aboutUS():
+    return render_template('AboutUs.htm')
+
 
 if __name__ == '__main__':
     app.run()
